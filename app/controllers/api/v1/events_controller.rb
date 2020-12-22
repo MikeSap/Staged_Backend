6cc582607f13848e3c496745601c0ff6da02959d
mike <mikesapienzaaudio@gmail.com>
2020-12-21 22:25:33 -0600

    refactored suggested

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -8,7 +8,6 @@ class Api::V1::EventsController < ApplicationController
 
     def create 
         event = Event.new(event_params)
-        byebug
         if params["photo"] == "null"
           #attach a default photo 
           event.photo.attach(io:File.open("app/assets/logo/staged_logo.png"), filename: "staged_logo.png", content_type: "image/png")

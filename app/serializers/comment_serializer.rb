d40d3dcaf1b41c66afe52b31600d512ee15bfc95
mike <mikesapienzaaudio@gmail.com>
2020-12-24 10:18:08 -0600

    added created to comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user, :event_id, :content
+  attributes :id, :user, :event_id, :content, :created_at
 
   def user
     u = User.find(object.user_id)

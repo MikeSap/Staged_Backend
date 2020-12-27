6914eafeabe85484505f1c2883e905a91ce1d384
mike <mikesapienzaaudio@gmail.com>
2020-12-27 17:21:23 -0600

    band create presence validations

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,6 +2,7 @@ class Band < ApplicationRecord
 
     validates :name, uniqueness: { case_sensitive: false }
     validates :url, uniqueness: { case_sensitive: false }
+    validates :name, :url, :bio, presence: true
 
     has_many :follows, foreign_key: :followed_id, class_name: "Connection"
     has_many :followers, through: :follows, source: :follower
